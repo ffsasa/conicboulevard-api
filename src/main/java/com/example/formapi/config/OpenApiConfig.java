607760c3fbf4d92ea26c144fe4a99392c5390bef
ffsasa <ngocan2003krp@gmail.com>
2025-09-24 10:31:14 +0700

    Update

--- src/main/java/com/example/formapi/config/OpenApiConfig.java
+++ src/main/java/com/example/formapi/config/OpenApiConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -22,6 +25,9 @@ public class OpenApiConfig {
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")))
-                .info(new Info().title("Conic Boulevard API").version("v1"));
+                .info(new Info().title("Conic Boulevard API").version("v1"))
+                .servers(List.of(
+                        new Server().url("https://api.conicboulevard.info")
+                ));
     }
 }
